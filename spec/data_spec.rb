b4c99df22f31c9af2fcb6838c2f7de801745d4d1
Ian Vaughan <github@ianvaughan.co.uk>
2013-03-15 18:47:14 +0000

    some specs uncommented, some new added

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -12,32 +12,44 @@ module Tickspot
       @data = Data.new h
     end
 
-    context 'a Client object' do
+    context 'Data should return Client objects' do
 
-      it 'should return an array of Clients' do
+      before do
         # input = [{'name'=>'foo', 'id'=>1}, {'name'=>'bar', 'id'=>2}] #['foo', 'bar']
         test_data = fixture 'clients_projects_tasks'
         test_client = Client.new test_data
         Api.any_instance.stub(:clients_projects_tasks).and_return(test_data) # stub out real API call
-        Client.should_receive(:new).with(test_data.first).and_return(test_client)
+        # Api.any_instance.stub(:clients_projects_tasks).and_return(test_client)
+      end
 
+      it 'should return a client names' do
+        @data.clients.names.should eq ['TestClient2', 'TestClient3']
+        @data.clients.first.name.should eq 'TestClient2'
+      end
+
+      it 'should return a client IDs' do
+        @data.clients.ids.should eq [128542, 888]
+        @data.clients.first.id.should eq 128542
+      end
+
+      it 'should return an array of Clients' do
+        Client.should_receive(:new).with(test_data.first).and_return(test_client)
         @data.clients.should eq [test_client]
       end
 
       it 'should yield each Client' do
-        # input = [{'name'=>'foo', 'id'=>1}, {'name'=>'bar', 'id'=>2}]  #['foo', 'bar']
-        test_data = fixture 'clients_projects_tasks'
-        test_client = Client.new test_data
-        Api.any_instance.stub(:clients_projects_tasks).and_return(test_client)
-
         @data.should_receive(:clients).and_yield test_client
         @data.clients { |c| }
       end
 
       it 'should return an array of projects' do
-        # test_data = fixture 'clients_projects_tasks'
-        # test_project = test_data[:projects]
-        # @data.clients.first.should eq [test_project]
+        test_data = fixture 'clients_projects_tasks'
+        test_project = test_data.first[:projects]
+
+        test_client = Client.new test_data
+        Api.any_instance.stub(:clients_projects_tasks).and_return(test_data)
+
+        @data.clients.first.projects.should eq [test_project]
       end
 
     end
