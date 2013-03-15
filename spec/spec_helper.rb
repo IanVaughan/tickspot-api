2ebfe91c43028e96df11242c831ed58ef5c6a1e9
Ian Vaughan <github@ianvaughan.co.uk>
2013-03-15 18:40:34 +0000

    load fixtures in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,13 @@
 require 'rspec'
 require 'rspec/autorun'
-
 require 'fakeweb'
 
 FakeWeb.allow_net_connect = false
 
 RSpec.configure do |c|
+  c.mock_with :rspec
 
+  def fixture filename
+    YAML.load_file File.join(File.dirname(__FILE__), 'fixtures', filename + '.yml')
+  end
 end
